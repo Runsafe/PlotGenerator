446fdb619a8f8e09d570de73c5a8893217d5f8d5
Kruithne <personal@stanleybatch.co.uk>
2012-03-03 18:10:24 +0000

    Comitted new version.

---
+++ src/me/Kruithne/KruGeneration/Blocks.java
@@ -0,0 +1,20 @@
+package me.Kruithne.KruGeneration;
+
+import org.bukkit.Material;
+
+class Blocks
+{
+	public static byte AIR = (byte)Material.AIR.getId();
+	public static byte BEDROCK = (byte)Material.BEDROCK.getId();
+	public static byte STONE = (byte)Material.STONE.getId();
+	public static byte GRASS = (byte)Material.GRASS.getId();
+	public static byte DIRT = (byte)Material.DIRT.getId();
+	public static byte STEP = (byte)Material.STEP.getId();
+	public static byte LOG = (byte)Material.LOG.getId();
+	public static byte LEAVES = (byte)Material.LEAVES.getId();
+	public static byte FENCE = (byte)Material.FENCE.getId();
+	public static byte TORCH = (byte)Material.TORCH.getId();
+	public static byte GLOWSTONE = (byte)Material.GLOWSTONE.getId();
+	public static byte WATER = (byte)Material.WATER.getId();
+	public static byte DOUBLESTEP = (byte)43;
+}
--- src/me/Kruithne/KruGeneration/KruGenerationGenerator.java
+++ src/me/Kruithne/KruGeneration/KruGenerationGenerator.java
@@ -1,189 +1,60 @@
 package me.Kruithne.KruGeneration;
 
+import java.util.Arrays;
 import java.util.Random;
 import org.bukkit.*;
 import org.bukkit.generator.ChunkGenerator;
 
 public class KruGenerationGenerator extends ChunkGenerator
 {
+	private int plotSize = 3;
+
+	private StraightRoad straight;
+	private CrossRoads intersect;
 
     public KruGenerationGenerator()
     {
+		straight = new StraightRoad();
+		intersect = new CrossRoads();
     }
 
     public byte[] generate(World world, Random random, int cx, int cz)
     {
         byte result[] = new byte[32768];
-        for(int x = 0; x < 16; x++)
-        {
-            for(int z = 0; z < 16; z++)
-            {
-                int height = 70;
-                for(int y = 0; y < height; y++)
-                {
-                    int block = (x * 16 + z) * 128 + y;
-                    int plotSize = 3;
-                    byte blockID = (byte)Material.AIR.getId();
-                    if(y == 64)
-                        blockID = (byte)Material.GRASS.getId();
-                    if(y == 0)
-                        blockID = (byte)Material.BEDROCK.getId();
-                    else
-                    if(y > 59 && y < 64)
-                        blockID = (byte)Material.DIRT.getId();
-                    else
-                    if(y < 60)
-                        blockID = (byte)Material.STONE.getId();
-                    if(cx % plotSize == 0 && cz % plotSize == 0)
-                    {
-                        if(x > 7 && x < 11 || z > 7 && z < 11)
-                            if(y == 63)
-                                blockID = (byte)Material.STONE.getId();
-                            else
-                            if(y == 64)
-                                blockID = (byte)Material.STEP.getId();
-                        if((x == 5 || x == 13) && (z < 6 || z > 12))
-                            if(y == 64)
-                                blockID = (byte)Material.LOG.getId();
-                            else
-                            if(y == 65 || y == 66)
-                                blockID = (byte)Material.LEAVES.getId();
-                        if((z == 5 || z == 13) && (x < 6 || x > 12))
-                            if(y == 64)
-                                blockID = (byte)Material.LOG.getId();
-                            else
-                            if(y == 65 || y == 66)
-                                blockID = (byte)Material.LEAVES.getId();
-                        if((x == 6 || x == 7 || x == 11 || x == 12) && (z < 8 || z > 10) && y == 65)
-                            blockID = (byte)Material.STEP.getId();
-                        if((z == 6 || z == 7 || z == 11 || z == 12) && (x < 8 || x > 10) && y == 65)
-                            blockID = (byte)Material.STEP.getId();
-                        if((x == 7 || x == 11) && (z < 8 || z > 10))
-                            if(y == 64)
-                                blockID = (byte)Material.AIR.getId();
-                            else
-                            if(y == 63)
-                                blockID = (byte)Material.WATER.getId();
-                            else
-                            if(y == 62)
-                                blockID = (byte)Material.STONE.getId();
-                        if((z == 7 || z == 11) && (x < 8 || x > 10))
-                            if(y == 64)
-                                blockID = (byte)Material.AIR.getId();
-                            else
-                            if(y == 63)
-                                blockID = (byte)Material.WATER.getId();
-                            else
-                            if(y == 62)
-                                blockID = (byte)Material.STONE.getId();
-                        if(x == 7 && z == 7 || x == 11 && z == 7 || x == 7 && z == 11 || x == 11 && z == 11)
-                            if(y == 65)
-                                blockID = 43;
-                            else
-                            if(y == 66)
-                                blockID = (byte)Material.FENCE.getId();
-                            else
-                            if(y == 67)
-                                blockID = (byte)Material.TORCH.getId();
-                        if((x == 6 || x == 12) && (z < 7 || z > 11) && (y == 64 || y == 63))
-                            blockID = (byte)Material.STONE.getId();
-                        if((z == 6 || z == 12) && (x < 7 || x > 11) && (y == 64 || y == 63))
-                            blockID = (byte)Material.STONE.getId();
-                        if((x == 6 || x == 12) && (z == 6 || z == 12) && y == 64)
-                            blockID = (byte)Material.GLOWSTONE.getId();
-                        if(x == 4 && z == 14 && y == 65)
-                            blockID = (byte)Material.LOG.getId();
-                    } else
-                    if(cx % plotSize == 0 && cz % plotSize != 0)
-                    {
-                        if(x > 7 && x < 11)
-                            if(y == 63)
-                                blockID = (byte)Material.STONE.getId();
-                            else
-                            if(y == 64)
-                                blockID = (byte)Material.STEP.getId();
-                        if(x == 5 || x == 13)
-                            if(y == 64)
-                                blockID = (byte)Material.LOG.getId();
-                            else
-                            if(y == 65 || y == 66)
-                                blockID = (byte)Material.LEAVES.getId();
-                        if((x == 6 || x == 7 || x == 11 || x == 12) && y == 65)
-                            blockID = (byte)Material.STEP.getId();
-                        if(x == 7 || x == 11)
-                            if(y == 64)
-                                blockID = (byte)Material.AIR.getId();
-                            else
-                            if(y == 63)
-                                blockID = (byte)Material.WATER.getId();
-                            else
-                            if(y == 62)
-                                blockID = (byte)Material.STONE.getId();
-                        if((x == 6 || x == 12) && (y == 64 || y == 63))
-                            blockID = (byte)Material.STONE.getId();
-                        if((x == 6 || x == 12) && (z == 5 || z == 13) && y == 64)
-                            blockID = (byte)Material.GLOWSTONE.getId();
-                        if((x == 7 || x == 11) && (z == 5 || z == 13))
-                            if(y == 65)
-                                blockID = 43;
-                            else
-                            if(y == 66)
-                                blockID = (byte)Material.FENCE.getId();
-                            else
-                            if(y == 67)
-                                blockID = (byte)Material.TORCH.getId();
-                    } else
-                    if(cx % plotSize != 0 && cz % plotSize == 0)
-                    {
-                        if(z > 7 && z < 11)
-                            if(y == 63)
-                                blockID = (byte)Material.STONE.getId();
-                            else
-                            if(y == 64)
-                                blockID = (byte)Material.STEP.getId();
-                        if(z == 5 || z == 13)
-                            if(y == 64)
-                                blockID = (byte)Material.LOG.getId();
-                            else
-                            if(y == 65 || y == 66)
-                                blockID = (byte)Material.LEAVES.getId();
-                        if((z == 6 || z == 7 || z == 11 || z == 12) && y == 65)
-                            blockID = (byte)Material.STEP.getId();
-                        if(z == 7 || z == 11)
-                            if(y == 64)
-                                blockID = (byte)Material.AIR.getId();
-                            else
-                            if(y == 63)
-                                blockID = (byte)Material.WATER.getId();
-                            else
-                            if(y == 62)
-                                blockID = (byte)Material.STONE.getId();
-                        if((z == 6 || z == 12) && (y == 64 || y == 63))
-                            blockID = (byte)Material.STONE.getId();
-                        if((z == 6 || z == 12) && (x == 5 || x == 13) && y == 64)
-                            blockID = (byte)Material.GLOWSTONE.getId();
-                        if((z == 7 || z == 11) && (x == 5 || x == 13))
-                            if(y == 65)
-                                blockID = 43;
-                            else
-                            if(y == 66)
-                                blockID = (byte)Material.FENCE.getId();
-                            else
-                            if(y == 67)
-                                blockID = (byte)Material.TORCH.getId();
-                    } else
-                    if(cx % plotSize != 0)
-                    {
-                        //int _tmp = cz % plotSize;
-                    }
-                    result[block] = blockID;
-                }
+		Arrays.fill(result, Blocks.AIR);
 
-            }
+		boolean hRoad = cx % plotSize == 0;
+		boolean vRoad = cz % plotSize == 0;
 
-        }
+		for(int x = 0; x < 16; ++x)
+		{
+			for(int z = 0; z < 16; ++z)
+			{
+				int offset = (x * 16 + z) * 128;
+				result[offset] = Blocks.BEDROCK;
+				Arrays.fill(result, offset +  1, offset + 60, Blocks.STONE);
+				Arrays.fill(result, offset + 60, offset + 64, Blocks.DIRT);
 
-        return result;
+				if(hRoad || vRoad)
+				{
+					for(int y = 0; y < 6; ++y)
+					{
+						byte what = 0;
+						if(hRoad && vRoad)
+							what = intersect.GetByte(x, y, z);
+
+						else
+							what = straight.GetByte(x, y, z, hRoad);
+
+						result[offset + 62 + y] = what;
+					}
+				}
+				else
+					result[offset + 64] = Blocks.GRASS;
+			}
+		}
+
+		return result;
     }
 
     public Location getFixedSpawnLocation(World world, Random random)
