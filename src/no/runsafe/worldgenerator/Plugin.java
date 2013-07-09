78a08464487d2aa3b0ea8cfe43f71803a35f4a34
Morten Nilsen <morten@runsafe.no>
2013-07-09 19:54:38 +0200

    Not working..

--- src/no/runsafe/worldgenerator/Plugin.java
+++ src/no/runsafe/worldgenerator/Plugin.java
@@ -7,11 +7,6 @@ public class Plugin extends RunsafePlugin
 {
 	public ChunkGenerator getDefaultWorldGenerator(String worldName, String id)
 	{
-		if (defaultGenerator == null)
-		{
-			defaultGenerator = super.getDefaultWorldGenerator(worldName, id);
-			getComponent(PlotChunkGenerator.class).setDefaultGenerator(defaultGenerator);
-		}
 		return getComponent(PlotChunkGenerator.class);
 	}
 
