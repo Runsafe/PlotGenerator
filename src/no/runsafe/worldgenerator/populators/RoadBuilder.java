0a7581991610fd3fb296fc696b4ce10b75eb20e7
Morten Nilsen <morten@runsafe.no>
2013-12-21 14:28:10 +0100

    Fix silly mistake and try once more

--- src/no/runsafe/worldgenerator/populators/RoadBuilder.java
+++ src/no/runsafe/worldgenerator/populators/RoadBuilder.java
@@ -26,26 +26,24 @@ public class RoadBuilder extends BlockPopulator
 		int X_FROM = 7;
 		int X_TO = X_FROM + crossection[0].length - 1;
 		int Y_FROM = 62;
-		int Y_TO = X_FROM + crossection.length - 1;
+		int Y_TO = Y_FROM + crossection.length - 1;
 		int Z_FROM = 1;
-		int Z_TO = Z_FROM + 16;
+		int Z_TO = Z_FROM + 15;
 		for (int x = X_FROM; x <= X_TO; ++x)
 			for (int y = Y_FROM; y <= Y_TO; ++y)
 				for (int z = Z_FROM; z <= Z_TO; ++z)
 				{
 					Block block = chunk.getBlock(flip ? z : x, y, flip ? x : z);
-					block.setType(crossection[2 - y + Y_FROM][x - X_FROM]);
-
-//					switch (block.getType())
-//					{
-//						case WATER:
-//						case AIR:
-//						case STEP:
-//							break;
-//						default:
-//							block.setType(crossection[2 - y + Y_FROM][x - X_FROM]);
-//							break;
-//					}
+					switch (block.getType())
+					{
+						case WATER:
+						case AIR:
+						case STEP:
+							break;
+						default:
+							block.setType(crossection[2 - y + Y_FROM][x - X_FROM]);
+							break;
+					}
 				}
 	}
 
