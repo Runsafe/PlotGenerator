86c7110e8366de6785ac08f1a8f848a8862a8561
Morten Nilsen <morten@runsafe.no>
2013-12-21 15:43:36 +0100

    Revert test

--- src/no/runsafe/worldgenerator/populators/Road.java
+++ src/no/runsafe/worldgenerator/populators/Road.java
@@ -7,7 +7,7 @@ public abstract class Road extends BlockPopulator
 {
 	protected final Material[][] crossectionbase = new Material[][]{
 		{Material.STEP, Material.STEP, Material.AIR, Material.AIR, Material.AIR, Material.STEP, Material.STEP},
-		{Material.STONE, Material.AIR, Material.WOOD_STEP, Material.WOOD_STEP, Material.WOOD_STEP, Material.AIR, Material.STONE},
+		{Material.STONE, Material.AIR, Material.STEP, Material.STEP, Material.STEP, Material.AIR, Material.STONE},
 		{Material.STONE, Material.WATER, Material.STONE, Material.STONE, Material.STONE, Material.WATER, Material.STONE},
 		{Material.STONE, Material.STONE, Material.STONE, Material.STONE, Material.STONE, Material.STONE, Material.STONE}
 	};
@@ -15,7 +15,7 @@ public abstract class Road extends BlockPopulator
 		{Material.AIR, Material.TORCH, Material.AIR, Material.AIR, Material.AIR, Material.TORCH, Material.AIR},
 		{Material.AIR, Material.FENCE, Material.AIR, Material.AIR, Material.AIR, Material.FENCE, Material.AIR},
 		{Material.STEP, Material.DOUBLE_STEP, Material.AIR, Material.AIR, Material.AIR, Material.DOUBLE_STEP, Material.STEP},
-		{Material.GLOWSTONE, Material.AIR, Material.WOOD_STEP, Material.WOOD_STEP, Material.WOOD_STEP, Material.AIR, Material.GLOWSTONE},
+		{Material.GLOWSTONE, Material.AIR, Material.STEP, Material.STEP, Material.STEP, Material.AIR, Material.GLOWSTONE},
 		{Material.STONE, Material.WATER, Material.STONE, Material.STONE, Material.STONE, Material.WATER, Material.STONE},
 		{Material.STONE, Material.STONE, Material.STONE, Material.STONE, Material.STONE, Material.STONE, Material.STONE}
 	};
