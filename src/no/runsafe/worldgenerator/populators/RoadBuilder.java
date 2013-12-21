1f75a6b246965c2ea2ea91aba05727324f15de1c
Morten Nilsen <morten@runsafe.no>
2013-12-21 14:29:33 +0100

    Off by one

--- src/no/runsafe/worldgenerator/populators/RoadBuilder.java
+++ src/no/runsafe/worldgenerator/populators/RoadBuilder.java
@@ -23,11 +23,11 @@ public class RoadBuilder extends BlockPopulator
 
 	private void apply(boolean flip, Chunk chunk)
 	{
-		int X_FROM = 7;
+		int X_FROM = 6;
 		int X_TO = X_FROM + crossection[0].length - 1;
 		int Y_FROM = 62;
 		int Y_TO = Y_FROM + crossection.length - 1;
-		int Z_FROM = 1;
+		int Z_FROM = 0;
 		int Z_TO = Z_FROM + 15;
 		for (int x = X_FROM; x <= X_TO; ++x)
 			for (int y = Y_FROM; y <= Y_TO; ++y)
